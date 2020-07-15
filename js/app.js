2465fc2e9c37d76f8f99bb7cd51da122168c4877
rajharsh81070 <rajharsh81070@gmail.com>
2020-07-16 00:35:19 +0530

    Final Changes

--- js/app.js
+++ js/app.js
@@ -1,45 +1,23 @@
 // build the nav
-const navList = document.getElementById('navbar__list');
-const navElements = document.querySelectorAll('section')
+const navElements = document.querySelectorAll('section');
+const navBar = document.getElementById('navbar__list');
 
-// Build menu by iterating through the navelements
-navElements.forEach(el => {
-  const navlistElement = `<li class='menu__link ${el.className}' data-link=${el.id}><a href="#${el.id}">${el.dataset.nav}</li>`
-  navList.insertAdjacentHTML('beforeend', navlistElement)
+// Build menu by iterating through the navElements
+navElements.forEach(navElement => {
+  const navListElement = document.createElement('li');
+  navListElement.className = 'menu__link';
+  navListElement.dataset.nav = navElement.id;
+  navListElement.innerText = navElement.dataset.nav;
+  navBar.appendChild(navListElement)
 })
 
-// Scroll to section on link click by listenting to the click-event in the navlist
-navList.addEventListener('click', e => {
-  e.preventDefault()
-  const parent = e.target.hasAttribute('data-link')
-    ? e.target
-    : e.target.parentElement
-  const elementToScrollTo = document.getElementById(parent.dataset.link)
-  elementToScrollTo.scrollIntoView({ block: 'end', behavior: 'smooth' })
+// Scroll to section on click.
+navBar.addEventListener('click', event => {
+  event.preventDefault();
+  const clicked = document.querySelector(`#${event.target.dataset.nav}`);
+  clicked.scrollIntoView({ behavior: 'smooth' })
 })
 
-// Set section and nav link as active using the IntersectionObserver pattern
-const callback = entries => {
-  entries.forEach(entry => {
-    const navListElement = document.querySelector(
-      `.menu__link[data-link='${entry.target.id}']`,
-    )
-    const section = document.getElementById(entry.target.id)
-
-    if (entry && entry.isIntersecting) {
-      navListElement.classList.add('active')
-      section.classList.add('active')
-    } else {
-      if (navListElement.classList.contains('active')) {
-        navListElement.classList.remove('active')
-      }
-
-      if (section.classList.contains('active')) {
-        section.classList.remove('active')
-      }
-    }
-  })
-}
 
 //Get the button
 const mybutton = document.getElementById("myBtn");
@@ -61,15 +39,39 @@ function topFunction() {
   document.documentElement.scrollTop = 0;
 }
 
-// Options for the observer. Most important is the threshold
-const options = {
-  root: null,
-  rootMargin: '0px',
-  threshold: 0.6,
+const sectionInView = () => {
+  const inViewport = () => {
+    let whichNavElement = navElements[0];
+    navElements.forEach(navElement => {
+      const boundary = navElement.getBoundingClientRect();
+      if (boundary.bottom >= -50 & boundary.top <= 50) {
+        whichNavElement = navElement;
+      }
+    })
+    return whichNavElement;
+  };
+
+  window.addEventListener(
+    "scroll",
+    function () {
+      let navElementSection = inViewport();
+      console.log(navElementSection);
+      navElementSection.classList.add("active");
+      navElements.forEach(navElement => {
+        if (navElement.id != navElementSection.id && navElement.classList.contains('active')) {
+          navElement.classList.remove('active');
+        }
+      });
+      const activeElement = document.querySelector(`li[data-nav="${navElementSection.id}"]`);
+      activeElement.classList.add('active__link');
+      const headerElements = document.querySelectorAll('.menu__link');
+      headerElements.forEach(headerElement => {
+        if (headerElement.dataset.nav != activeElement.dataset.nav & headerElement.classList.contains('active__link')) {
+          headerElement.classList.remove('active__link');
+        }
+      })
+    }
+  );
 }
 
-// Setting an observer with options and a callback which checks if the navelement should be active
-const observer = new IntersectionObserver(callback, options)
-navElements.forEach(el => {
-  observer.observe(document.getElementById(el.id))
-})
+sectionInView();
